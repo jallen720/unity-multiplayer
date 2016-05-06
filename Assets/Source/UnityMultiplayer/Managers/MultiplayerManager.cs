ef7aa43bd073ca51bef3e43217df036412366876
Joel Allen <joelallen720@gmail.com>
2016-05-06 11:50:40 -0700

    Add main menu for signing the player in before starting a game. Add basic sign-in functionality.

---
+++ Assets/Source/UnityMultiplayer/Managers/MultiplayerManager.cs
@@ -0,0 +1,42 @@
+﻿using GooglePlayGames;
+using UnityEngine;
+using UnityUtils.Managers;
+using USceneManager = UnityEngine.SceneManagement.SceneManager;
+
+namespace UnityMultiplayer.Managers {
+    public class MultiplayerManager : Singleton<MultiplayerManager> {
+        private PlayGamesPlatform playGamesPlatform;
+
+        public MultiplayerManager() {
+            PlayGamesPlatform.DebugLogEnabled = true;
+            PlayGamesPlatform.Activate();
+            playGamesPlatform = PlayGamesPlatform.Instance;
+        }
+
+        // Static members
+
+        public static void SignInAndStartMPGame() {
+            if (!Instance.playGamesPlatform.localUser.authenticated) {
+                Instance.playGamesPlatform.localUser.Authenticate(HandleAuthentication);
+            }
+            else {
+                Debug.Log("You're already signed in.");
+                StartGame();
+            }
+        }
+
+        private static void HandleAuthentication(bool authenticatedSuccessfully) {
+            if (authenticatedSuccessfully) {
+                Debug.Log("We're signed in! Welcome " + Instance.playGamesPlatform.localUser.userName);
+                StartGame();
+            }
+            else {
+                Debug.Log("Oh... we're not signed in.");
+            }
+        }
+
+        private static void StartGame() {
+            USceneManager.LoadScene("Game");
+        }
+    }
+}
---
+++ Assets/Source/UnityMultiplayer/UI/PlayButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityMultiplayer.Managers;
+
+namespace UnityMultiplayer.UI {
+    
+    [RequireComponent(typeof(Button))]
+    public class PlayButton : MonoBehaviour {
+        private void Start() {
+            GetComponent<Button>().onClick.AddListener(MultiplayerManager.SignInAndStartMPGame);
+        }
+    }
+}
