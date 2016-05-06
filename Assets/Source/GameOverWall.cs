7eed58d7173a1f17c9356ef0a4f01f3c2304c98e
Joel Allen <joelallen720@gmail.com>
2016-05-06 11:04:25 -0700

    Move scripts to UnityMultiplayer namespace.

--- Assets/Source/GameOverWall.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class GameOverWall : MonoBehaviour {
-    private void OnTriggerEnter2D(Collider2D collider) {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
-}
---
+++ Assets/Source/UnityMultiplayer/GameOverWall.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UnityMultiplayer {
+    public class GameOverWall : MonoBehaviour {
+        private void OnTriggerEnter2D(Collider2D collider) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
